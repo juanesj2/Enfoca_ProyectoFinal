fcbc17f4824107a3a65c02ec3e1a3a74f22107a6
juanesj2 <juanstivenaa82@gmail.com>
2026-05-26 14:37:09 +0200

    Descargar fotos de los albums

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\SwipeQuestion;
+use App\Models\DrawingPrompt;
 
 class GameSeeder extends Seeder
 {
