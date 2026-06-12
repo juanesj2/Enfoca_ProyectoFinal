297ddbaab19b3014231b6817a58136f65819de7d
jsalcanizTCN <jstiven@tecon.es>
2026-06-12 11:02:19 +0200

    Add app column to users table for widget tracking

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,12 +18,14 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
+            'app' => 'nullable|string|in:enfoca,love_widget',
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'app' => $request->app ?? 'enfoca',
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,7 @@ class User extends Authenticatable
         'telefono',
         'estado',
         'current_mood',
+        'app',
     ];
 
     /**
