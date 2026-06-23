ffffad4a40d037042443770bc9da09194e11ef71
jsalcanizTCN <jstiven@tecon.es>
2026-06-23 12:47:58 +0200

    Fix broken Leaflet marker icons by explicitly importing images

--- src/Pages/Fotografias/Comentar.jsx
+++ src/Pages/Fotografias/Comentar.jsx
@@ -6,6 +6,17 @@ import MainLayout from '../../Layouts/MainLayout';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+});
+
 // Componente pequeño para el mapa de Leaflet
 const LeafletMap = ({ lat, lng }) => {
     useEffect(() => {
